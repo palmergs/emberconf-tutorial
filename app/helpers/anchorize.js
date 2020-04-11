a95fa5f464e82246af7df61b9b2b4bb2a853e913
Galen Palmer <palmergs@gmail.com>
2020-04-11 15:07:40 -0400

    missing helper

---
+++ app/helpers/anchorize.js
@@ -0,0 +1,10 @@
+import { helper } from '@ember/component/helper';
+
+export default helper(function anchorize(params/*, hash*/) {
+  return params[0]
+    .toLowerCase()
+    .replace(".", "")
+    .replace("-", "")
+    .split(" ")
+    .join("-");
+});
